5f470b95a455f1cbe63f27eeaae48993b8d01715
Zaahid Mohammed Mukarram <36190083+mohammedzaahid@users.noreply.github.com>
2018-11-15 16:10:37 +0530

    Version 2.0
    Updated with errors fixed and few functionalities added. (previous code was not fulfilling few requirements such as considering longest prefix for minimum price) 
    Provision for CSV Excel file file to be added where file path needs to be pasted is mentioned as a comment but can be easily converted to statement.

--- TeleRout.py
+++ TeleRout.py
@@ -1,27 +1,29 @@
-import pandas as pd     #package used to implement data frames for operator and price lists.
-import math             #This library is used to split telephone number into its prefixes to match with operator lists
-import unittest         #package imported for performing unit tests with regards to code and data provided
-
-
-class DFTests(unittest.TestCase):
-    def setUp(self, df):
-        try:
-            my_df1 = df1
-            my_df2 = df2
-            expected_df1 = pd.DataFrame({'Operator': [1, 268, 46, 4620, 468, 4631, 4673, 46732], 'Cost': [0.9, 5.1, 0.17, 0.0, 0.15, 0.15, 0.9, 1.1]})
-            expected_df2 = pd.DataFrame({'Operator': [1, 44, 46, 467, 48], 'Cost': [0.92, 0.5, 0.2, 1.0, 1.2]})
-            """addition of data frames in 'main' leading to more expected data frames for unit tests"""
-            pd.testing.assert_frame_equal(my_df1, expected_df1)             #matching data frame to data frame in 'main'
-            pd.testing.assert_frame_equal(my_df2, expected_df2)
-        except Exception as e:
-            print("Unit Test failed ", e)
+import pandas as pd		 											#package used to implement data frames for operator and price lists.
+import math             											#This library is used to split telephone number into its prefixes to match with operator lists
+import unittest			 											#package imported for performing unit tests with regards to code and data provided
+from pandas.util.testing import assert_frame_equal					#function to conduct unit tests and match my dataframes with expected dataframes
 
 
 def first_n_digits(num1, n):
-    return num1 // 10 ** (int(math.log(num1, 10)) - n + 1)
+    return num1 // 10 ** (int(math.log(num1, 10)) - n + 1)			#Telephone Number is split with formula to obtain respective prefixes to match with data given
 
 
-def func2(dat):
+class DFTests(unittest.TestCase):									#defining functions for unit tests to conduct on code
+    def setUp(self, df1, df2):
+        try:														#try used to catch and handle exceptions during unit tests after matching data i.e. Operator and price lists
+            my_df1 = df1											#command statement for my_df1, my_df2 as data to match with given data in code
+            my_df2 = df2
+            #expected_df1=pd.read_csv("filepath",sep='\t')			#command to import excel file as CSV with operators and price lists 
+            #expected_df2=pd.read_csv("filepath",sep='\t')			#remove hashtag and paste path of file in place of filepath
+            expected_df1 = pd.DataFrame({'Operator': [1, 268, 46, 4620, 468, 4631, 4673, 46732], 'Cost': [0.9, 5.1, 0.17, 0.0, 0.15, 0.15, 0.9, 1.1]})	#expected_df1 defines data which is to be matched with data in body of code
+            expected_df2 = pd.DataFrame({'Operator': [1, 44, 46, 467, 48], 'Cost': [0.92, 0.5, 0.2, 1.3, 1.2]})
+            pd.testing.assert_frame_equal(my_df1, expected_df1)		#match dataframes in body of code with expected dataframes
+            pd.testing.assert_frame_equal(my_df2, expected_df2)
+        except Exception as e:										#handle case when data is not matched with data in body of code
+            print("UNIT TEST FAILED", e)							#print output as unit test failed with percentage amount of error
+
+
+def func2(dat):														
     try:
         for key, value in dat.items():
             for name, values in value.iterrows():
@@ -44,28 +46,24 @@ def func2(dat):
 
 if __name__ == '__main__':
     DFTests1 = DFTests()
-    num1 = input("Enter the Telephone Number ")
-    if num1.startswith('+'):        #if user inputs telephone number with +, it is disregarded
-        num1 = num1[1:]
+    num1 = input('Enter the Telephone Number ')						 #assigning input as num1 which is telephone number
+    if num1.startswith('+'):										 #if statement where case is input given with +
+        num1 = num1[1:]												 #statement to disregard + if input given as +TelephoneNumber
     else:
         num1 = num1
-    num1 = int(num1)
-
-    df1 = pd.DataFrame({'Operator': [1, 268, 46, 4620, 468, 4631, 4673, 46732], 'Cost': [0.9, 5.1, 0.17, 0.0, 0.15, 0.15, 0.9, 1.1]})
-    df2 = pd.DataFrame({'Operator': [1, 44, 46, 467, 48], 'Cost': [0.92, 0.5, 0.2, 1.0, 1.2]})
-    """Operator and price list can be added here."""
-    dfs1 = {'Operator A ': df1}         #naming data frames as particular operators
-    dfs2 = {'Operator B ': df2}
-    DFTests1.setUp(df1)
-    DFTests1.setUp(df2)
-    y = func2(dfs1)
+    num1 = int(num1)                                                #assigning num1 as integer for matching prefix
+    df1 = pd.DataFrame({'Operator': [1, 268, 46, 4620, 468, 4631, 4673, 46732], 'Cost': [0.9, 5.1, 0.17, 0.0, 0.15, 0.15, 0.9, 1.1]})       #assigned dataframes as Operators and Price Lists in pandas package
+    df2 = pd.DataFrame({'Operator': [1, 44, 46, 467, 48], 'Cost': [0.92, 0.5, 0.2, 1.3, 1.2]})
+    dfs1 = {'Routed by Operator A': df1}                            #assigning names as per required output to dataframes to conduct tests
+    dfs2 = {'Routed by Operator B': df2}
+    DFTests1.setUp(df1, df2)                                        #calling DFTests function to perform test to match prefix in dataframes
+    y = func2(dfs1)                                                 #assigning y and z variable to defined dataframes
     z = func2(dfs2)
-
-    if y is None:
+    if y is None:                                                   #statement to find out match in dfs1, Operator A
         c = z
-    elif z is None:
+    elif z is None:                                                 #statement to find out match in dfs2, Operator B
         c = y
-    else:
-        c = min(y, z)
+    else:                                                           #when match is found in both operators
+        c = min(y, z)                                               #to calculate minimum of both matches
 
-    print(c)
+    print("Minimum price is", c)
